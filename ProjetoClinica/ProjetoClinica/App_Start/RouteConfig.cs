a9a2d38df43785fb14555eaec4541cd543f9dd84
Caroline Ribeiro de Freitas <carolinerfreitas6@gmail.com>
2017-11-09 11:15:06 -0200

    Criação do Controller

--- ProjetoClinica/ProjetoClinica/App_Start/RouteConfig.cs
+++ ProjetoClinica/ProjetoClinica/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ProjetoClinica
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Clinicas", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ProjetoClinica/ProjetoClinica/Controllers/ClinicasController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoClinica.Models;
+
+namespace ProjetoClinica.Controllers
+{
+    public class ClinicasController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Clinicas
+        public ActionResult Index()
+        {
+            return View(db.Clinicas.ToList());
+        }
+
+        // GET: Clinicas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clinica clinica = db.Clinicas.Find(id);
+            if (clinica == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clinica);
+        }
+
+        // GET: Clinicas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clinicas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ClinicaId,ClinicaNome,ClinicaMedico,Login,Senha")] Clinica clinica)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clinicas.Add(clinica);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(clinica);
+        }
+
+        // GET: Clinicas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clinica clinica = db.Clinicas.Find(id);
+            if (clinica == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clinica);
+        }
+
+        // POST: Clinicas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ClinicaId,ClinicaNome,ClinicaMedico,Login,Senha")] Clinica clinica)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clinica).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(clinica);
+        }
+
+        // GET: Clinicas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clinica clinica = db.Clinicas.Find(id);
+            if (clinica == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clinica);
+        }
+
+        // POST: Clinicas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Clinica clinica = db.Clinicas.Find(id);
+            db.Clinicas.Remove(clinica);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        // GET: Clinicas/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Clinicas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([Bind(Include = "ClinicaId,ClinicaNome,ClinicaMedico,Login,Senha")] Clinica clinica)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(clinica);
+        }
+    }
+}
